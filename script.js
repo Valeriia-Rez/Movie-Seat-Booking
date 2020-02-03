ad9930def86dbeb32a42c96c48eef6f9d8f4e783
Valeriia-Rez <Valeria09122016@gmail.com>
2020-02-04 00:12:52 +0200

    Save data to local storage

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = parseInt(movieSelect.value);
 
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
@@ -15,8 +24,11 @@ function updateSelectedCount() {
 
 movieSelect.addEventListener("change", e => {
     ticketPrice = parseInt(e.target.value);
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
-})
+});
+
+
 
 container.addEventListener("click", e => {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
